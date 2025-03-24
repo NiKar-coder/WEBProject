c7da470542cccd8a530f8af990a7e9d11eb3e0fb
NiKar-coder <NikitaKarataev22@gmail.com>
2025-03-24 16:10:35 +0300

    Add addPage.html

--- server.py
+++ server.py
@@ -11,5 +11,13 @@ def index():
         return render_template('index.html', result="")
 
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        return render_template('add.html')
+    else:
+        return render_template('addPage.html')
+
+
 if __name__ == '__main__':
     app.run(host='192.168.1.2', port='8080', debug=True)
