086d06e0af1b39b4efb9eeacb63e9cda4de6a35d
NiKar-coder <NikitaKarataev22@gmail.com>
2025-03-24 23:48:43 +0300

    Add search

--- data/owners.py
+++ data/owners.py
@@ -5,10 +5,10 @@ from .db_session import SqlAlchemyBase
 class Owner(SqlAlchemyBase):
     __tablename__ = 'owners'
 
-    number = sqlalchemy.Column(sqlalchemy.Integer,
-                               primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String)
+    carsNumber = sqlalchemy.Column(sqlalchemy.String,
+                               primary_key=True)
     surname = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String)
     patronymic = sqlalchemy.Column(sqlalchemy.String)
     flat = sqlalchemy.Column(sqlalchemy.String)
     phone = sqlalchemy.Column(sqlalchemy.String)
--- server.py
+++ server.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, request
 from data import db_session
+from data.owners import Owner
 
 db_session.global_init("db/CarNumbers.db")
+# owner = Owner()
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
     if request.method == 'POST':
-        return render_template('index.html', result="Результат поиска: "
-                               + "dd")
-    else:
-        return render_template('index.html', result="")
+        if 'searchBtn' in request.form:
+            try:
+                db_sess = db_session.create_session()
+                input_ = request.form['input_']
+                for owner in db_sess.query(Owner).filter(Owner.carsNumber
+                                                         == input_):
+                    res = f"ФИО: {owner.surname} {owner.name} {owner.patronymic}, квартира: {owner.flat}, телефон: {owner.phone}, модель машины: {owner.carsModel}"
+                db_sess.close()
+                return render_template('index.html', result=res)
+            except Exception:
+                return render_template('index.html', result="")
 
 
 @app.route('/add', methods=['GET', 'POST'])
