f25476008a165f1bc5d1f556446d0fa121525d36
NiKar-coder <NikitaKarataev22@gmail.com>
2025-03-28 15:51:58 +0300

    Add admins mantgtmtnt

--- server.py
+++ server.py
@@ -2,9 +2,31 @@ from flask import Flask, render_template, request, url_for, redirect
 from data import db_session
 from data.owners import Owner
 import json
+import os
 
 db_session.global_init("db/CarNumbers.db")
 app = Flask(__name__)
+
+admins = {
+    "admins": [
+        {
+            "login": "admin",
+            "password": "admin",
+            "db": "true",
+            "excluded": "true",
+            "admins": "true"
+        }
+    ]
+}
+if not os.path.exists('admins.json'):
+    with open('admins.json', 'w+') as admins_file:
+        json.dump(admins, admins_file, ensure_ascii=False)
+excluded = {
+    "excluded": ["о000оо"]
+}
+if not os.path.exists('excluded.json'):
+    with open('excluded.json', 'w+') as excluded_file:
+        json.dump(excluded, excluded_file, ensure_ascii=False)
 
 
 def redirect_():
@@ -45,6 +67,103 @@ def index():
             return render_template('index.html', res="false", alert="false")
     else:
         return render_template('index.html', res="false", alert="false")
+
+
+@app.route('/editAdmins_login', methods=['GET', 'POST'])
+def editAdmins_login():
+    if request.method == 'POST':
+        try:
+            login = request.form['loginInput']
+            password = request.form['passwordInput']
+            with open('admins.json', 'r') as file:
+                admins = json.load(file)
+            for el in admins['admins']:
+                if login == el['login'] and password == el['password']\
+                        and el['admins'] == "true":
+                    return render_template('editAdmins_login.html',
+                                           alert="true",
+                                           login="true",
+                                           message="Вы успешно вошли"
+                                           )
+            return render_template('editAdmins_login.html', alert="true",
+                                   message="Неверный логин или пароль",
+                                   login="false")
+        except Exception:
+            return render_template('editAdmins_login.html', alert="true",
+                                   message="Ошибка входа",
+                                   login="false")
+    else:
+        return render_template('editAdmins_login.html', alert="false",
+                               login="false")
+
+
+@app.route('/editAdmins')
+def editAdmins():
+    return render_template('editAdmins.html')
+
+
+@app.route('/addAdmin', methods=['GET', 'POST'])
+def addAdmin():
+    if request.method == 'POST':
+        try:
+            login = request.form['loginInput']
+            password = request.form['passwordInput']
+            try:
+                request.form['DB_access']
+                DB_access = "true"
+            except Exception:
+                DB_access = "false"
+            try:
+                request.form['BL_access']
+                BL_access = "true"
+            except Exception:
+                BL_access = "false"
+            try:
+                request.form['admins_access']
+                admins_access = "true"
+            except Exception:
+                admins_access = "false"
+            with open('admins.json', 'r') as file:
+                admins = json.load(file)
+                admins['admins'].append({
+                    "login": login,
+                    "password": password,
+                    "db": DB_access,
+                    "excluded": BL_access,
+                    "admins": admins_access
+                })
+            with open('admins.json', 'w') as file:
+                json.dump(admins, file, ensure_ascii=False)
+            return render_template('addAdmin.html', alert="true",
+                                   message=f"Админ {login} успешно добавлен")
+        except Exception:
+            return render_template('addAdmin.html', alert="true",
+                                   message="Ошибка добавления")
+    else:
+        return render_template('addAdmin.html', alert="false")
+
+
+@app.route('/removeAdmin', methods=['GET', 'POST'])
+def removeAdmin():
+    if request.method == 'POST':
+        try:
+            input_ = request.form['input_']
+            with open('admins.json', 'r') as file:
+                admins = json.load(file)
+
+            list_ = admins['admins']
+            for el in list_:
+                if el['login'] == input_:
+                    list_.remove(el)
+            with open('admins.json', 'w') as file:
+                json.dump(admins, file, ensure_ascii=False)
+            return render_template('removeAdmin.html', alert="true",
+                                   message=f"Админ {input_} успешно удален")
+        except Exception:
+            return render_template('removeAdmin.html', alert="true",
+                                   message="Ошибка удаления")
+    else:
+        return render_template('removeAdmin.html', alert="false")
 
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -93,19 +212,19 @@ def DB_login():
             for el in admins['admins']:
                 if login == el['login'] and password == el['password']\
                         and el['db'] == "true":
-                    return render_template('login.html', alert="true",
+                    return render_template('DB_login.html', alert="true",
                                            login="true",
                                            message="Вы успешно вошли"
                                            )
-            return render_template('login.html', alert="true",
+            return render_template('DB_login.html', alert="true",
                                    message="Неверный логин или пароль",
                                    login="false")
         except Exception:
-            return render_template('login.html', alert="true",
+            return render_template('DB_login.html', alert="true",
                                    message="Ошибка входа",
                                    login="false")
     else:
-        return render_template('login.html', alert="false",
+        return render_template('DB_login.html', alert="false",
                                login="false")
 
 
